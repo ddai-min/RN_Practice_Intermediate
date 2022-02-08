cf78e7a3dff1068ef2b28c2a3c0c9733074fb6fe
taek <yoda3708@gmail.com>
2022-02-08 15:30:46 +0900

    add : authLogo, authForm

---
+++ diary_app/app/components/auth/authForm.js
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+import React, { Component } from 'react'
+import { StyleSheet, View, Text } from 'react-native'
+
+class AuthForm extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Hello</Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({})
+
+export default AuthForm
--- diary_app/app/components/auth/index.js
+++ diary_app/app/components/auth/index.js
@@ -6,18 +6,53 @@
  * @flow strict-local
  */
 import React, { Component } from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import {
+  StyleSheet,
+  View,
+  Text,
+  ActivityIndicator,
+  ScrollView
+} from 'react-native'
+import AuthLogo from './authLogo'
+import AuthForm from './authForm'
 
 class AuthComponent extends Component {
+  state = {
+    loading: false
+  }
+
   render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Auth Screen</Text>
-      </View>
-    )
+    if (this.state.loading) {
+      return (
+        <View style={styles.loading}>
+          <ActivityIndicator />
+        </View>
+      )
+    } else {
+      return (
+        <ScrollView style={styles.container}>
+          <View>
+            <AuthLogo />
+            <AuthForm />
+          </View>
+        </ScrollView>
+      )
+    }
   }
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  loading: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#7487C5',
+    padding: 30
+  }
+})
 
 export default AuthComponent
