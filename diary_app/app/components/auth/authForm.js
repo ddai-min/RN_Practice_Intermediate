0935e0f366c65639edcf2a7fb9c9fbff3589aeba
taek <yoda3708@gmail.com>
2022-02-08 16:31:36 +0900

    add : textInputEvent

--- diary_app/app/components/auth/authForm.js
+++ diary_app/app/components/auth/authForm.js
@@ -11,48 +11,108 @@ import Input from '../../utils/forms/input'
 
 class AuthForm extends Component {
   state = {
-    myTextInput: ''
+    type: 'Register',
+    action: 'Login',
+    actionMode: '새로 등록할게요.',
+    hasErrors: true,
+    form: {
+      email: {
+        value: '',
+        type: 'textinput',
+        rules: {},
+        valid: false
+      },
+      password: {
+        value: '',
+        type: 'textinput',
+        rules: {},
+        valid: false
+      },
+      confirmPassword: {
+        value: '',
+        type: 'textinput',
+        rules: {},
+        valid: false
+      }
+    }
   }
+
+  updateInput = (name, value) => {
+    this.setState({
+      hasErrors: false
+    })
+
+    let formCopy = this.state.form
+    formCopy[name].value = value
+
+    this.setState({
+      form: formCopy
+    })
+
+    console.warn(this.state.form)
+  }
+
+  confirmPassword = () =>
+    this.state.type != 'Login' ? (
+      <Input
+        value={this.state.form.confirmPassword.value}
+        type={this.state.form.confirmPassword.type}
+        secureTextEntry={true}
+        placeholder="비밀번호 재입력"
+        placeholderTextColor="#ddd"
+        onChangeText={value => this.updateInput('confirmPassword', value)}
+      />
+    ) : null
+
+  formHasErrors = () =>
+    this.state.hasErrors ? (
+      <View style={styles.errorContainer}>
+        <Text style={styles.errorLabel}>로그인 정보를 다시 확인해주세요.</Text>
+      </View>
+    ) : null
 
   render() {
     return (
       <View>
         <Input
-          value={this.state.myTextInput}
+          value={this.state.form.email.value}
+          type={this.state.form.email.type}
           autoCapitalize={'none'}
           keyboardType={'email-address'}
           placeholder="이메일 주소"
           placeholderTextColor="#ddd"
+          onChangeText={value => this.updateInput('email', value)}
         />
 
         <Input
-          value={this.state.myTextInput}
-          autoCapitalize={'none'}
-          keyboardType={'email-address'}
+          value={this.state.form.password.value}
+          type={this.state.form.password.type}
+          secureTextEntry={true}
           placeholder="비밀번호"
           placeholderTextColor="#ddd"
+          onChangeText={value => this.updateInput('password', value)}
         />
 
-        <Input
-          value={this.state.myTextInput}
-          autoCapitalize={'none'}
-          keyboardType={'email-address'}
-          placeholder="비밀번호 재확인"
-          placeholderTextColor="#ddd"
-        />
+        {this.confirmPassword()}
+        {this.formHasErrors()}
       </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
-  input: {
-    width: '100%',
-    borderBottomWidth: 1,
-    borderBottomColor: '#fff',
-    fontSize: 17,
-    padding: 5,
-    marginTop: 30
+  errorContainer: {
+    marginBottom: 10,
+    marginTop: 30,
+    padding: 20,
+    backgroundColor: '#ee3344'
+  },
+  errorLabel: {
+    color: '#fff',
+    fontSize: 15,
+    fontWeight: 'bold',
+    textAlignVertical: 'center',
+    textAlign: 'center'
   }
 })
 
--- diary_app/app/utils/forms/input.js
+++ diary_app/app/utils/forms/input.js
@@ -1,13 +1,35 @@
 import React from 'react'
 import { StyleSheet, TextInput } from 'react-native'
 
-const input = props => <TextInput {...props} style={styles.input} />
+const input = props => {
+  let template = null
+  switch (props.type) {
+    case 'textinput':
+      template = <TextInput {...props} style={styles.input} />
+      break
+    case 'textinputRevised':
+      template = <TextInput {...props} style={styles.inputRevised} />
+      break
+
+    default:
+      return template
+  }
+  return template
+}
 
 const styles = StyleSheet.create({
   input: {
     width: '100%',
     borderBottomWidth: 1,
     borderBottomColor: '#fff',
+    fontSize: 17,
+    padding: 5,
+    marginTop: 30
+  },
+  inputRevised: {
+    width: '100%',
+    borderBottomWidth: 3,
+    borderBottomColor: 'red',
     fontSize: 17,
     padding: 5,
     marginTop: 30
