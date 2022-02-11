6604d3716856c6cf1a29eff23bf519ae97996ee3
taek <yoda3708@gmail.com>
2022-02-11 16:13:41 +0900

    add : signIn

--- diary_app/app/store/actions/user_actions.js
+++ diary_app/app/store/actions/user_actions.js
@@ -1,15 +1,33 @@
 import { SIGN_IN, SIGN_UP } from '../types'
 
 import axios from 'axios'
-import { SIGNUP } from '../../utils/misc'
+import { SIGNUP, SIGNIN } from '../../utils/misc'
 
 export function signIn(data) {
+  const request = axios({
+    method: 'POST',
+    url: SIGNIN,
+    data: {
+      email: data.email,
+      password: data.password,
+      returnSecureToken: true
+    },
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(response => {
+      console.log(response.data)
+      return response.data
+    })
+    .catch(err => {
+      alert('에러 발생')
+      return false
+    })
+
   return {
     type: SIGN_IN,
-    payload: {
-      email: data.email,
-      token: data.password
-    }
+    payload: request
   }
 }
 
--- diary_app/app/utils/misc.js
+++ diary_app/app/utils/misc.js
@@ -1,2 +1,3 @@
 export const APIKEY = `AIzaSyBNHSCgrM72KSDni-eNQtynKtlaJWurXZw`
 export const SIGNUP = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${APIKEY}`
+export const SIGNIN = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${APIKEY}`
