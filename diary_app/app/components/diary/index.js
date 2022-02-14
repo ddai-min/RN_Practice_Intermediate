64f1d228f5257048eb37fd3a87304c0068040e20
taek <yoda3708@gmail.com>
2022-02-14 17:03:13 +0900

    add : diaryDocuButton

--- diary_app/app/components/diary/index.js
+++ diary_app/app/components/diary/index.js
@@ -6,9 +6,20 @@
  * @flow strict-local
  */
 import React, { Component } from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import {
+  StyleSheet,
+  View,
+  Text,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+  Dimensions
+} from 'react-native'
 import { connect } from 'react-redux'
 import { getDiaries } from '../../store/actions/diary_actions'
+
+const screenHeight = Dimensions.get('window').height
+const screenWidth = Dimensions.get('window').width
 
 class DiaryComponent extends Component {
   componentDidMount() {
@@ -17,8 +28,26 @@ class DiaryComponent extends Component {
 
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Diary Screen</Text>
+      <View>
+        <ScrollView style={{ backgroundColor: '#f0f0f0' }}></ScrollView>
+
+        <TouchableOpacity
+          style={{
+            position: 'absolute',
+            left: screenWidth * 0.8,
+            top: screenHeight * 0.7
+          }}
+          onPress={() => {
+            this.props.navigation.navigate('DiaryDocu', {
+              newDiary: true
+            })
+          }}>
+          <Image
+            source={require('../../assets/images/pen_circle.png')}
+            style={{ width: 50, height: 50 }}
+            resizeMode="contain"
+          />
+        </TouchableOpacity>
       </View>
     )
   }
