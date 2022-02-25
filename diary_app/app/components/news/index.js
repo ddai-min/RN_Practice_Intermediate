e8c18ff07c90e9d8d4f7e0abe8a5cffe889b9e81
taek <yoda3708@gmail.com>
2022-02-25 18:24:44 +0900

    add : publicAPIData

--- diary_app/app/components/news/index.js
+++ diary_app/app/components/news/index.js
@@ -7,8 +7,74 @@
  */
 import React, { Component } from 'react'
 import { StyleSheet, View, Text, Image } from 'react-native'
+import axios from 'axios'
 
 class NewsComponent extends Component {
+  state = {
+    covid: {
+      dateTime: '',
+      confirmed: 0, // 확진환자
+      confirmedDailyChange: 0, // 확진환자 변화량
+      released: 0, // 격리해제
+      releasedDailyChange: 0, // 격리해제 일일 변화량
+      deceased: 0, // 사망자
+      deceasedDailyChange: 0, // 사망자 일일 변화량
+      inProgress: 0, // 검사진행
+      inProgressDailyChange: 0 // 검사진행 일일 변화량
+    },
+    dust: {
+      place: '서울',
+      dateTime: '',
+      fineDust: 0, // 미세먼지
+      fineDustLevel: '', // 미세먼지 단계
+      ultraFineDust: 0, // 초미세먼지
+      ultraFineDustLevel: '', // 초미세먼지 단계
+      nitrogenDioxide: 0, // 이산화질소
+      nitrogenDioxideLevel: '' // 이산화질소 단계
+    }
+  }
+
+  componentDidMount() {
+    let today = this.formatDate().today
+    let yesterday = this.formatDate().yesterday
+
+    const requestCovid = axios({
+      method: 'GET',
+      url: `http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson?serviceKey=SL%2F9mRlGvCNOkjtzP2xB3oddIVzVqnMIIG9Vi7kPGFkbM2IzMfCzsABRFGfTyM4OkPbsGOgA4a5WwSfiIDIN%2BA%3D%3D&pageNo=1&numOfRows=10&startCreateDt=${yesterday}&endCreateDt=${today}`
+    }).then(response => {
+      this.makeCovidData(response.data)
+    })
+  }
+
+  formatDate = () => {
+    let todayDate = new Date()
+    let today = this.calculateDate(todayDate)
+
+    let yesterdayDate = new Date(Date.now() - 86400000)
+    // 86400000 = 24 * 60 * 60 * 1000
+    let yesterday = this.calculateDate(yesterdayDate)
+
+    let dateData = {
+      today: today,
+      yesterday: yesterday
+    }
+
+    return dateData
+  }
+
+  calculateDate = date => {
+    let year = date.getFullYear()
+    let month = (date.getMonth() + 1).toString()
+    let day = date.getDate().toString()
+
+    if (month.length < 2) month = `0${month}`
+    if (day.length < 2) day = `0${day}`
+
+    let finalDate = `${year}${month}${day}`
+
+    return finalDate
+  }
+
   render() {
     return (
       <View style={styles.newsContainer}>
